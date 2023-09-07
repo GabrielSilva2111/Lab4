eadab5e38be6acb907aa0646473684472daa46f4
Parralol <santinico36@hotmail.com>
2023-09-07 18:52:43 -0500

    "Implementacion pruebas "

---
+++ LigthwayContainer_DependencyInjection_Hangman/src/main/java/hangman/Test/GameScoreTest.java
@@ -0,0 +1,56 @@
+package hangman.Test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import hangman.model.GameScore;
+import hangman.model.OriginalScore;
+
+public class GameScoreTest {
+
+    
+    @Test
+    public void OriginalScoreShouldntBeNegative(){
+        //Arrange
+        GameScore prueba = new OriginalScore();
+        //Act
+        int res = prueba.calculateScore(0, 13);
+        Assert.assertTrue(res>=0);
+
+    }
+
+    @Test
+    public void OriginalShouldSubstract(){
+        
+    }
+
+    @Test
+    public void BonusShouldAdd(){
+        
+    }
+
+    @Test
+    public void BonusShouldSubstract(){
+        
+    }
+
+    @Test
+    public void PowerBonusShouldntBeNegative(){
+        
+    }
+
+    @Test
+    public void PowerBonusShouldAdd(){
+        
+    }
+
+    @Test
+    public void PowerBonusShouldSubstract(){
+        
+    }
+
+    @Test
+    public void PowerBonusShouldBeLessThanFiveHundreed(){
+        
+    }
+}
