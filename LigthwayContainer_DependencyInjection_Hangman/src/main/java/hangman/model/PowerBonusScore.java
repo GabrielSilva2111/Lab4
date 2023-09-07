83c73b2eb11d13c23682afb33485461b78dc6e43
Parralol <santinico36@hotmail.com>
2023-09-07 18:05:28 -0500

    "especificacion metodos"

---
+++ LigthwayContainer_DependencyInjection_Hangman/src/main/java/hangman/model/PowerBonusScore.java
@@ -0,0 +1,21 @@
+package hangman.model;
+import java.math.*;
+public class PowerBonusScore implements GameScore {
+    int puntaje = 0;
+     /**
+     * @pre Posee 0 puntos
+     * @pos Puede acabar minimo con 0 puntos o maximo 500, se penaliza con 5 puntos y bonifica con 10
+     * @param correctCount var entero
+     * @param incorrectCount var entero
+     * @return puntaje
+     */
+    public int calculateScore(int correctCount, int incorrectCount) {
+        
+        puntaje += Math.pow(5, correctCount);
+        puntaje -= incorrectCount*8;
+        if(puntaje >=500) return 500;
+        if(puntaje< 0) return 0;
+        return puntaje;
+    }
+    
+}
