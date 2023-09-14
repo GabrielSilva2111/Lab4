ecc47602381fc0548ef32581d54ef9030ee56580
Parralol <110953563+Parralol@users.noreply.github.com>
2023-09-13 20:53:36 -0500

    "Implementacion pruebas"

--- LigthwayContainer_DependencyInjection_Hangman/src/test/java/hangman/model/GameScoreTest.java
+++ LigthwayContainer_DependencyInjection_Hangman/src/test/java/hangman/model/GameScoreTest.java
@@ -32,31 +32,61 @@ public class GameScoreTest {
 
     @Test
     public void BonusShouldAdd(){
-        
+        //Arrange
+        GameScore prueba = new BonusScore();
+        //Act
+        int res = prueba.calculateScore(10, 1);
+        //Assert
+        Assert.assertTrue(res>=0);
     }
 
     @Test
     public void BonusShouldSubstract(){
-        
+        //Arrange
+        GameScore prueba = new BonusScore();
+        //Act
+        int res = prueba.calculateScore(1, 1);
+        //Assert
+        Assert.assertTrue(res>=5);
     }
 
     @Test
     public void PowerBonusShouldntBeNegative(){
-        
+        //Arrange
+        GameScore prueba = new PowerBonusScore();
+        //Act
+        int res = prueba.calculateScore(0, 13);
+        //Assert
+        Assert.assertTrue(res>=0);
     }
 
     @Test
     public void PowerBonusShouldAdd(){
-        
+                //Arrange
+        GameScore prueba = new PowerBonusScore();
+        //Act
+        int res = prueba.calculateScore(1, 0);
+        //Assert
+        Assert.assertTrue(res==5);
     }
 
     @Test
     public void PowerBonusShouldSubstract(){
-        
+                //Arrange
+        GameScore prueba = new PowerBonusScore();
+        //Act
+        int res = prueba.calculateScore(2, 1);
+        //Assert
+        Assert.assertTrue(res==17);
     }
 
     @Test
     public void PowerBonusShouldBeLessThanFiveHundreed(){
-        
+                //Arrange
+        GameScore prueba = new PowerBonusScore();
+        //Act
+        int res = prueba.calculateScore(13, 0);
+        //Assert
+        Assert.assertTrue(res==500);
     }
 }
